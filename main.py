b199436ca305fe27976e04e3670e3af2b70e4d2c
connordeane20 <60219200+connordeane20@users.noreply.github.com>
2020-09-20 13:49:46 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+
+print("CAlCULATOR")
+num_1 = input ("Enter First Number: ")
+math = input("Choose Your Equation: ")
+num_2 = input("Enter Second Number: ")
+if math == "/":
+    print(int(num_1) / int(num_2))
+if math == "*":
+    print(int(num_1) * int(num_2))
+if math == "+":
+    print(int(num_1) + int(num_2))
+if math == "-":
+    print(int(num_1) - int(num_2))
+
+
